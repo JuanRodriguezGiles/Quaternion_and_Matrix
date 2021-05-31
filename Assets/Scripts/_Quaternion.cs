e688abb0358d9257800b8d98e9259b134e917676
Juan <juanrodriguezgiles@gmail.com>
2021-05-31 10:31:27 -0300

    Created unity project and scripts

---
+++ Assets/Scripts/_Quaternion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+public struct _Quaternion : IEquatable<_Quaternion>
+{
+    //private Quaternion q;
+    float x;
+    float y;
+    float z;
+    float w;
+
+
+
+
+
+
+
+
+
+
+    public bool Equals(_Quaternion other)
+    {
+        throw new NotImplementedException();
+    }
+}
